2edd6851039e469e3a4475559800b67f3e1e55c6
Nate Smith <me@nwlsmith.com>
2019-04-22 13:35:10 -0400

    week 11

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('title')
+    Your Books
+@endsection
+
+@section('content')
+    <h1> Your Books </h1>
+    <p>
+        Your books will go here.
+    </p>
+@endsection
